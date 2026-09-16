a4833f8d73aa24cce42aa879b2e4d53286eed27a
MahatVasudev <mahatvasudev9@gmail.com>
2026-09-16 13:17:17 +0530

    BugFixed for DataSet BERT

--- Backend/app/pipeline/autoencoder.py
+++ Backend/app/pipeline/autoencoder.py
@@ -1,12 +1,14 @@
+from ml_factory.models.autoencoder import BaseNormalAutoEncoder, NormalityAE
+
 from app.pipeline import PipelinePhase
 from app.type_store import Phase, PhaseInput, Result, SuccessForReview, SuccessReturn
 from app.type_store._error import PhaseError
-from ml_factory.models.autoencoder import BaseNormalAutoEncoder, NormalityAE
 
 
 class AutoEncoderPipeline(PipelinePhase):
     def __init__(self, model_weights):
         super().__init__(phase=Phase("autoencoder"))
+        ...
 
     def verdict(self, input: PhaseInput) -> Result[SuccessReturn, PhaseError]:
         return super().verdict(input)
